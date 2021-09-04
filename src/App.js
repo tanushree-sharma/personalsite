4788916ab1d0f045997e3e506a0003fcc3badd06
tanushree-sharma <tanushree@hotmail.ca>
2021-09-04 18:21:27 -0500

    deleted text

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
+//import Footer from './Components/Footer';
 import About from './Components/About';
-import Resume from './Components/Resume';
-import Portfolio from './Components/Portfolio';
+//import Resume from './Components/Resume';
+//import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
@@ -45,10 +45,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,17 +23,14 @@ class Header extends Component {
 
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
          </ul>
 
       </nav>
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Hi! I'm {name}.</h1>
-            <h3>I'm a {occupation} based in <span> {city} </span>. {description}.</h3>
+            <h1 className="responsive-headline"> {name}</h1>
+            <h3> {occupation} <span> {city} </span>{description}.</h3>
             <hr />
             <ul className="social">
                {networks}
